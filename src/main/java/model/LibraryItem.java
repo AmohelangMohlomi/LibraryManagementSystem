f06df9392b3d60d11d14dc1a7b2334004fe1d99f
Amohelang Mohlomi <mohlomiamohelang3@gmail.com>
2025-10-01 23:39:47 +0200

    Added methods for the LibraryItem class

--- src/main/java/model/LibraryItem.java
+++ src/main/java/model/LibraryItem.java
@@ -1,4 +1,32 @@
 package model;
 
-public class LibraryItem {
+public abstract class LibraryItem {
+
+    private String id;
+    private String title;
+    private boolean isAvailable;
+
+    public LibraryItem(String id, String title, boolean isAvailable){
+        this.id = id;
+        this.title= title;
+        this.isAvailable=isAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public abstract void displayInfo();
 }
