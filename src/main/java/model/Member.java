3e8aa89b30b2a618523ee3be89550e59f1156700
Amohelang Mohlomi <mohlomiamohelang3@gmail.com>
2025-10-02 00:06:12 +0200

    Added the member classes

--- src/main/java/model/Member.java
+++ src/main/java/model/Member.java
@@ -1,4 +1,49 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Member {
+
+    private String memberId;
+    private String name;
+
+    private ArrayList<LibraryItem> borrowedItems;
+
+    public Member(String memberId, String name){
+        this.memberId=memberId;
+        this.name=name;
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public void borrowItem(LibraryItem item){
+        if(item.isAvailable()){
+            borrowedItems.add(item);
+            item.setAvailable(false);
+        }else{
+            System.out.println("Item is not available");
+        }
+    }
+
+    public void returnItem(LibraryItem item){
+        if(borrowedItems.contains(item)){
+            borrowedItems.remove(item);
+            item.setAvailable(true);
+        }else{
+            System.out.println("Item wa snot borrowed by the member");
+        }
+
+    }
+
+    public void displayBorrowedItems(String[] args) {
+
+        if (borrowedItems.isEmpty()){
+            System.out.println("No Items borrowed");
+        }else{
+            System.out.println("======Borrowed Items=====");
+            for(LibraryItem item: borrowedItems){
+                System.out.println(item);
+            }
+        }
+    }
+
 }
